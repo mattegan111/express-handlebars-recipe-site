8636e2d1b110efd11656643cefc3ae2192a3ede4
Matthew Egan <69609987+mattegan111@users.noreply.github.com>
2022-08-10 16:09:37 +1200

    Setup home page using express-handlebars

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 //Loads the express module
-const express = require('express')
+const express = require('express');
 //Creates our express server
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
+//Loads the handlebars module
+const handlebars = require('express-handlebars')
+//Sets our app to use the handlebars engine
+app.set('view engine', 'handlebars')
+//Sets handlebars configurations (we will go through them later on)
+app.engine('handlebars', handlebars.engine({
+  layoutsDir: __dirname + '/views/layouts',
+}))
 //Serves static files (we need it to import a css file)
 app.use(express.static('public'))
-//Sets a basic route
-app.get('/', (req, res) => res.send('Hello World !'))
+//Serves the body of the page aka "main.handlebars" to the container //aka "index.handlebars"
+app.get('/', (req, res) => {
+  res.render('main', {layout : 'index'});
+})
 
 //Makes the app listen to port 3000
-app.listen(port, () => console.log(`App listening to port ${port}`))
+app.listen(port, () => console.log(`App listening to port ${port}`));
